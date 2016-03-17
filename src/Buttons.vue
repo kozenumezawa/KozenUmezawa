4e82fd81cb91b0b884f6f7096c7d3f884f7d9bfe
Takao NAITO <xarsh@outlook.com>
2016-03-17 19:58:40 +0900

    Remove unused buttons.

--- src/Buttons.vue
+++ src/Buttons.vue
@@ -2,11 +2,7 @@
 ._buttons
   .row
     .column.column-50: a.button(@click="emit('apply')") Apply
-    .column.column-25: a.button Undo
-    .column.column-25: a.button Redo
-  .row
     .column.column-50: a.button(@click="emit('reset')") Reset
-    .column.column-50: a.button Save
 </template>
 
 <script>
