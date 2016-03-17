e7e0c5d7edb7453a10df18770ee99d707ed8af15
Takao NAITO <xarsh@outlook.com>
2016-03-18 01:20:38 +0900

    Implement a basic vertex shader which can be used for PBR.

--- src/shader.js
+++ src/shader.js
@@ -1,36 +1,54 @@
 module.exports = {
-  fragmentShader: [
-    "uniform vec3 diffuse;",
-    "uniform float opacity;",
+  vertexShader: [
+    "attribute float radius;",
 
-    THREE.ShaderChunk[ "common" ],
-    THREE.ShaderChunk[ "color_pars_fragment" ],
-    THREE.ShaderChunk[ "map_particle_pars_fragment" ],
-    THREE.ShaderChunk[ "fog_pars_fragment" ],
-    THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
-    THREE.ShaderChunk[ "logdepthbuf_pars_fragment" ],
+    THREE.ShaderChunk["common"],
+    THREE.ShaderChunk["color_pars_vertex"],
+    THREE.ShaderChunk["shadowmap_pars_vertex"],
+    THREE.ShaderChunk["logdepthbuf_pars_vertex"],
 
     "void main() {",
 
+    THREE.ShaderChunk["color_vertex"],
+    THREE.ShaderChunk["begin_vertex"],
+    THREE.ShaderChunk["project_vertex"],
+
+    // THREE.ShaderChunk["logdepthbuf_vertex"],
+    // THREE.ShaderChunk["worldpos_vertex"],
+    // THREE.ShaderChunk["shadowmap_vertex"],
+
+    "  gl_PointSize = radius * 5.0;",
+
+    "}",
+  ].join("\n"),
+
+  fragmentShader: [
+    "uniform vec3 diffuse;",
+
+    THREE.ShaderChunk["common"],
+    THREE.ShaderChunk["color_pars_fragment"],
+    // THREE.ShaderChunk["map_particle_pars_fragment"],
+    // THREE.ShaderChunk["fog_pars_fragment"],
+    // THREE.ShaderChunk["shadowmap_pars_fragment"],
+    // THREE.ShaderChunk["logdepthbuf_pars_fragment"],
+
+    "void main() {",
     "  vec3 n;",
     "  n.xy = gl_PointCoord * 2.0 - 1.0;",
     "  n.z = 1.0 - dot( n.xy, n.xy );",
     "  if ( n.z < 0.0 ) discard;",
 
-    "  vec3 outgoingLight = vec3( 0.0 );",
-    "  vec4 diffuseColor = vec4( diffuse, opacity );",
+    "  vec4 diffuseColor = vec4(diffuse, 1.0);",
 
-    THREE.ShaderChunk[ "logdepthbuf_fragment" ],
-    THREE.ShaderChunk[ "map_particle_fragment" ],
-    THREE.ShaderChunk[ "color_fragment" ],
-    THREE.ShaderChunk[ "alphatest_fragment" ],
+    // THREE.ShaderChunk["logdepthbuf_fragment"],
+    // THREE.ShaderChunk["map_particle_fragment"],
+    THREE.ShaderChunk["color_fragment"],
+    // THREE.ShaderChunk["alphatest_fragment"],
 
-    "	 outgoingLight = diffuseColor.rgb;",
+    // THREE.ShaderChunk["fog_fragment"],
 
-    THREE.ShaderChunk[ "fog_fragment" ],
-
-    "  gl_FragColor = vec4( outgoingLight, diffuseColor.a );",
+    "  gl_FragColor = diffuseColor;",
 
     "}"
-  ].join( "\n" )
+  ].join("\n")
 }
