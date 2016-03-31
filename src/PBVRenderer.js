3591aa2cbdb66b9d966f81ea584165fa7fd75f60
Takao NAITO <xarsh@outlook.com>
2016-03-31 13:47:52 +0900

    Refactoring: Split a large and complicated rendering program.

---
+++ src/PBVRenderer.js
@@ -0,0 +1,110 @@
+import _ from 'lodash';
+
+import THREE from 'three';
+import Stats from 'stats.js';
+const OrbitControls = require('three-orbit-controls')(THREE);
+
+import shader from './shader';
+
+export default class PBVRenderer {
+  constructor (width, height) {
+    this.animate = this.animate.bind(this);
+
+    this.stats = new Stats();
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
+    this.camera.position.x = 80;
+
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+    this.renderer.setSize(width, height);
+
+    this.geometry = new THREE.BufferGeometry();
+
+    this.material = new THREE.ShaderMaterial(_.assign(THREE.ShaderLib['points'], {
+      uniforms: {
+        alphaZero: {type: 'f', value: 0.3},
+        rZero: {type: 'f', value: 0.9}
+      },
+      vertexColors: THREE.VertexColors,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader
+    }));
+
+    this.points = new THREE.Points(this.geometry, this.material);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.kvsml = {values: [], maxValue: 0, minValue: 1, numberOfVertices: 0};
+  }
+
+  animate () {
+    requestAnimationFrame(this.animate);
+    this.controls.update();
+    this.stats.update();
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  getMaxValue () {
+    return this.kvsml.maxValue;
+  }
+
+  getMinValue () {
+    return this.kvsml.minValue;
+  }
+
+  getNumberOfVertices () {
+    return this.kvsml.numberOfVertices;
+  }
+
+  getFramesPerSecond () {
+    return this.stats.domElement.innerText.slice(0, 2);
+  }
+
+  getDomElement () {
+    return this.renderer.domElement;
+  }
+
+  setVertexCoords (coords) {
+    this.kvsml.numberOfVertices = coords.length / 3;
+    this.geometry.addAttribute('position', new THREE.BufferAttribute(coords, 3));
+  }
+
+  setVertexValues (values) {
+    this.kvsml.maxValue = _.max(values);
+    this.kvsml.minValue = _.min(values);
+    this.kvsml.values = values;
+  }
+
+  addPointsToScene () {
+    this.scene.add(this.points);
+  }
+
+  updateVertexColors (spectrum) {
+    const colors = new Float32Array(_.flatMap(this.kvsml.values, v => {
+      return spectrum[Math.floor(100 * (v - this.kvsml.minValue) / this.kvsml.maxValue)];
+    }));
+    this.geometry.addAttribute('color', new THREE.BufferAttribute(colors, 4));
+  }
+
+  updateVertexRadius (radius) {
+    const radiuses = new Float32Array(_.map(this.kvsml.values, v => {
+      const idx = Math.floor(radius.length * (v - this.kvsml.minValue) / this.kvsml.maxValue);
+      return radius[idx];
+    }));
+    this.geometry.addAttribute('radius', new THREE.BufferAttribute(radiuses, 1));
+  }
+
+  updateOpacityParams (alphaZero, rZero) {
+    this.material.uniforms.alphaZero.value = alphaZero;
+    this.material.uniforms.rZero.value = rZero;
+  }
+
+  updateAllAttriutes (params) {
+    this.updateVertexColors(params.spectrum);
+    this.updateVertexRadius(params.radius);
+    this.updateOpacityParams(params.alphaZero, params.rZero);
+  }
+}
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -7,148 +7,87 @@ warning(:show.sync="warningVisible")
     | Number of vertices: <b>{{ numberOfVertices }}</b><br>
     | Frames per second: <b>{{ framesPerSecond }}</b>
   .column.column-50
-    | Maximum value: <b>{{ Math.floor(maxValue * 100) / 100 }}</b><br>
-    | Minimum value: <b>{{ Math.floor(minValue * 100) / 100 }}</b>
+    | Maximum value: <b>{{ maxValue }}</b><br>
+    | Minimum value: <b>{{ minValue }}</b>
 </template>
 
 <script>
-import _ from 'lodash';
-
-import THREE from 'three';
-import Stats from 'stats.js';
-const OrbitControls = require('three-orbit-controls')(THREE);
-
 import request from 'axios';
 request.defaults.responseType = 'arraybuffer';
 
-import Warning from './Warning.vue';
-import shader from '../shader';
+import PBVRenderer from '../PBVRenderer';
 
-let camera, scene, renderer, controls, geometry, material, points, stats;
+import Warning from './Warning.vue';
+
+const pbvr = new PBVRenderer(640, 640);
 
 export default {
   components: {
     Warning
   },
   ready () {
-    this.init();
-    this.animate();
+    pbvr.animate();
 
     this.$on('updateVertexColors', () => {
-      if(this.$parent.applyImmediately) this.updateVertexColors();
+      if(this.$parent.applyImmediately){
+        pbvr.updateVertexColors(this.$parent.spectrum);
+      }
     });
 
     this.$on('updateVertexRadius', () => {
-      if(this.$parent.applyImmediately) this.updateVertexRadius();
+      if(this.$parent.applyImmediately){
+        pbvr.updateVertexRadius(this.$parent.radius);
+      }
     });
 
     this.$on('updateOpacityParams', () => {
-      if(this.$parent.applyImmediately) this.updateOpacityParams();
+      if(this.$parent.applyImmediately){
+        pbvr.updateOpacityParams(this.$parent.alphaZero, this.$parent.rZero);
+      }
     });
 
-    this.$on('apply', () => this.updateAttriutes());
-    this.$on('reset', () => this.updateAttriutes());
+    this.$on('apply', () => pbvr.updateAllAttriutes(this.$parent));
+    this.$on('reset', () => pbvr.updateAllAttriutes(this.$parent));
+
+    document.getElementById('result').appendChild(pbvr.getDomElement());
+
+    this.retrieveSampleKvsml();
   },
   data () {
     return {
       framesPerSecond: 0,
-      kvsml: {normal: [], coord: [], value: []},
+      minValue: 0,
+      maxValue: 0,
+      numberOfVertices: 0,
       warningVisible: false
     }
   },
-  computed: {
-    el: () => document.getElementById('result'),
-    maxValue () { return _.max(this.kvsml.value) },
-    minValue () { return _.min(this.kvsml.value) },
-    numberOfVertices () { return this.kvsml.coord.length / 3 }
-  },
   methods: {
-    init () {
-      stats = new Stats();
-
-      scene = new THREE.Scene();
-
-      camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-      camera.position.x = 80;
-
-      renderer = new THREE.WebGLRenderer({antialias: true});
-      renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
-      renderer.setSize(this.el.offsetWidth, this.el.offsetWidth);
-
-      geometry = new THREE.BufferGeometry();
-
-      material = new THREE.ShaderMaterial(_.assign(THREE.ShaderLib['points'], {
-        uniforms: {
-          alphaZero: {type: 'f', value: this.$parent.alphaZero},
-          rZero: {type: 'f', value: this.$parent.rZero}
-        },
-        vertexColors: THREE.VertexColors,
-        vertexShader: shader.vertexShader,
-        fragmentShader: shader.fragmentShader
-      }));
-
-      points = new THREE.Points(geometry, material);
-
-      controls = new OrbitControls(camera, renderer.domElement);
-
-      this.retrieveSampleKvsml();
-
-      this.el.appendChild(renderer.domElement);
-    },
-    animate () {
-      requestAnimationFrame(this.animate);
-      controls.update();
-      stats.update();
-      this.framesPerSecond = stats.domElement.innerText.slice(0, 2);
-      renderer.render(scene, camera);
-    },
-    updateAttriutes () {
-      this.updateVertexColors();
-      this.updateVertexRadius();
-      this.updateOpacityParams();
-    },
     retrieveSampleKvsml () { // TODO: This block should be replaced with OPeNDAP request if needed.
       request.get('./assets/kvsml/test_coord.dat')
-      .then(res => {
-        this.kvsml.coord = new Float32Array(res.data);
-        this.updateVertexCoords();
-      })
+      .then(res => pbvr.setVertexCoords(new Float32Array(res.data)))
       .then(() => request.get('./assets/kvsml/test_value.dat'))
-      .then(res => {
-        this.kvsml.value = new Float32Array(res.data);
-      })
+      .then(res => pbvr.setVertexValues(new Float32Array(res.data)))
       .then(() => {
-        if(this.kvsml.coord.length / 3 > 1000000){
+        if(pbvr.getNumberOfVertices() > 1000000){
           this.warningVisible = true;
         } else {
           this.addPointsToScene();
         }
       })
-      .then(() => this.updateAttriutes())
-      .catch(console.error);
+      .then(() => pbvr.updateAllAttriutes(this.$parent))
+      .then(() => this.updateStats())
     },
     addPointsToScene () {
-      scene.add(points);
+      pbvr.addPointsToScene();
     },
-    updateVertexCoords () {
-      geometry.addAttribute('position', new THREE.BufferAttribute(this.kvsml.coord, 3));
-    },
-    updateVertexColors () {
-      const colors = new Float32Array(_.flatMap(this.kvsml.value, v => {
-        return this.$parent.spectrum[Math.floor((v - this.minValue) / this.maxValue * 100)];
-      }));
-      geometry.addAttribute('color', new THREE.BufferAttribute(colors, 4));
-    },
-    updateVertexRadius () {
-      const radiuses = new Float32Array(_.map(this.kvsml.value, v => {
-        const idx = Math.floor(this.$parent.radius.length * (v - this.minValue) / this.maxValue);
-        return this.$parent.radius[idx];
-      }));
-      geometry.addAttribute('radius', new THREE.BufferAttribute(radiuses, 1));
-    },
-    updateOpacityParams () {
-      material.uniforms.alphaZero.value = this.$parent.alphaZero;
-      material.uniforms.rZero.value = this.$parent.rZero;
+    updateStats () {
+      this.minValue = Math.floor(pbvr.getMinValue() * 100) / 100;
+      this.maxValue = Math.floor(pbvr.getMaxValue() * 100) / 100;
+      this.numberOfVertices = pbvr.getNumberOfVertices();
+      setInterval(() => {
+        this.framesPerSecond = pbvr.getFramesPerSecond();
+      }, 1000);
     }
   }
 }
